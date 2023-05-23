b10ab4531f02c604d75d9e4af0bcfde1e85254b9
student-2 <student2@planetrobotics.com>
2023-05-23 18:52:20 -0400

    first

---
+++ CelsiusToFahrenheit.py
@@ -0,0 +1,7 @@
+#program to convert Celsius to Fahrenheit
+#f=c*9/5+32
+print ('Enter temperature in Celsius')
+c=input()
+Celsius=float(c)
+F=Celsius*9/5+32
+print(f'Temperature in Fahrenheit is {F}')
